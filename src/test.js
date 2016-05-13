525e9229714e5e654f22ef0cf101c93995c2db5a
anyoyam <fenyam@outlook.com>
2016-05-13 18:42:14 +0800

    学习ECMAScript6

--- src/test.js
+++ src/test.js
@@ -1,1 +1,15 @@
 let [a, b] = [1, 2];
+console.log(a, b);
+
+function f() {console.log('I am outside!');}
+(function() {
+	if (false) {
+		//重复声明一次函数f
+		function f() {
+			console.log('I am inside!');
+		}
+	}
+	f();
+}());
+
+
